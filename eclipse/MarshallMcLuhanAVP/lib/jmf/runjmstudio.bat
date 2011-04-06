91e7b5e7519d751eb95e011e516190e38514140e
unknown <kehl@.(none)>
2011-04-05 20:50:06 -0600

    Removed extra folder

--- eclipse/MarshallMcLuhanAVP/lib/jmf/runjmstudio.bat
+++
@@ -1,3 +0,0 @@
-@set JAVA_CMD="%JAVA_HOME%\bin\java"
-@if "%JAVA_HOME%"== "" SET JAVA_CMD="java"
-%JAVA_CMD% -classpath fobs4jmf.jar;jmf.jar JMStudio
--- eclipse/MarshallMcLuhanAVP/src/com/physics/Transform2D.java
+++
@@ -1,5 +0,0 @@
-package com.physics;
-
-public interface Transform2D {
-	public Vec2D transform(Vec2D point);
-}
